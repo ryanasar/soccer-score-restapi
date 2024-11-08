43adf79e2e4d215ea364206a32c8bf062ccc44e7
Ryan Asar <ryanasar05@gmail.com>
2024-11-07 23:47:28 -0600

    hopefully this works

--- db.js
+++ db.js
@@ -5,9 +5,9 @@ const Pool = require('pg').Pool;
 const pool = new Pool({
     user: 'postgres',
     host: 'soccer-score-db.cd64e0e6k491.us-east-1.rds.amazonaws.com',
-    database: 'appuser',
+    database: 'postgres',
     password: 'matuidipsg',
-    port: 5432
+    port: 9816
 })
 
 module.exports = pool;
