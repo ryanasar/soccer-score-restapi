074f7b66daf06dbb610ae3f227d25c73fb1ef55e
Ryan Asar <ryanasar05@gmail.com>
2024-11-08 10:29:28 -0600

    change port to 3000

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const appUserRoutes = require('./src/appuser/routes.js')
 
 const app = express()
-const port = 3001
+const port = 3000
 const cors = require('cors');
 
 // Enable CORS for all origins
