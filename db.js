7168df069436af1a88eb52b99ec69750a216cd72
Ryan Asar <ryanasar05@gmail.com>
2024-11-08 09:49:32 -0600

    changed to local db

--- db.js
+++ db.js
@@ -4,10 +4,10 @@ const Pool = require('pg').Pool;
 
 const pool = new Pool({
     user: 'postgres',
-    host: 'soccer-score-db.cd64e0e6k491.us-east-1.rds.amazonaws.com',
+    host: 'localhost',
     database: 'postgres',
     password: 'matuidipsg',
-    port: 9816
+    port: 5433
 })
 
 module.exports = pool;
